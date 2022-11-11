68b6db23497f4213c9ec4982f58fff70c6f02660
Hoàng Đăng <thanhoangdang@gmail.com>
2022-11-11 22:08:00 +0700

    Fix UI

--- frontend/src/pages/forum/thread/Thread.jsx
+++ frontend/src/pages/forum/thread/Thread.jsx
@@ -38,65 +38,9 @@ function Thread() {
                   className="avatar"
                 />
                 <div className="user-info">
-                  <Link>Rena Ryuugu</Link>
-                  <span>Member</span>
-                </div>
-              </div>
-
-              <div className="message">
-                <time dateTime="2022-10-09 19:00" className="message-head">
-                  09/10/2022 at 19:00
-                </time>
-                <div className="message-body">
-                  <p>Hauu. Omochi Kaeri</p>
-                </div>
-                <div className="message-foot">
-                  <Link>
-                    <ReplyIcon />
-                    <span>Reply</span>
-                  </Link>
-                </div>
-              </div>
-            </article>
-
-            <article>
-              <div className="user">
-                <img
-                  src={require("../../../images/avatar-test.jpg")}
                   alt=""
-                  className="avatar"
-                />
                 <div className="user-info">
-                  <Link>Rena Ryuugu</Link>
-                  <span>Member</span>
-                </div>
-              </div>
-
-              <div className="message">
-                <time dateTime="2022-10-09 19:00" className="message-head">
-                  09/10/2022 at 19:00
-                </time>
-                <div className="message-body">
-                  <p>Hauu. Omochi Kaeri</p>
-                </div>
-                <div className="message-foot">
-                  <Link>
-                    <ReplyIcon />
-                    <span>Reply</span>
-                  </Link>
-                </div>
-              </div>
-            </article>
-
-            <article>
-              <div className="user">
-                <img
-                  src={require("../../../images/avatar-test.jpg")}
-                  alt=""
-                  className="avatar"
-                />
-                <div className="user-info">
-                  <Link>Rena Ryuugu</Link>
+                  <Link to="/forum/user/user_id">Rena Ryuugu</Link>
                   <span>Member</span>
                 </div>
               </div>
