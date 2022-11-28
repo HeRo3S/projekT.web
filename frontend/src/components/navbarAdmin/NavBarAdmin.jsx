339be7b6765c6d05c5dc06fa334f8e40baedcdac
Hoàng Đăng <thanhoangdang@gmail.com>
2022-11-28 22:53:58 +0700

    Nav Bar Admin

---
+++ frontend/src/components/navbarAdmin/NavBarAdmin.jsx
@@ -0,0 +1,43 @@
+import "./navbaradmin.css";
+import { Link } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import ReplyIcon from "@mui/icons-material/Reply";
+
+function NavBarAdmin() {
+  return (
+    <div id="nav-bar-admin">
+      <span>Admin Page</span>
+      <div id="user-nav">
+        <span>Forum</span>
+        <Link className="user-nav-block">
+          <PersonIcon className="icon" />
+          <span>Member</span>
+        </Link>
+        <Link className="user-nav-block">
+          <AdminPanelSettingsIcon className="icon" />
+          <span>Admin</span>
+        </Link>
+        <Link to="/forum" className="user-nav-block">
+          <ReplyIcon className="icon" />
+          <span>Back to forum</span>
+        </Link>
+      </div>
+
+      <div id="admin-profile">
+        <span>Profile</span>
+        <div id="profile-content">
+          <img src={require("../../images/avatar-admin.jpg")} alt="" />
+          <div id="admin-name">
+            <span>Rika Furude</span>
+            <span>Forum's Manager</span>
+          </div>
+        </div>
+      </div>
+
+      <button>Log out</button>
+    </div>
+  );
+}
+
+export default NavBarAdmin;
--- frontend/src/pages/forum/admin/Admin.jsx
+++ frontend/src/pages/forum/admin/Admin.jsx
@@ -1,9 +1,19 @@
 import "./admin.css";
+import NavBarAdmin from "../../../components/navbarAdmin/NavBarAdmin";
 
 function Admin() {
   return (
     <div id="admin" className="main">
-      a
+      <NavBarAdmin />
+      <div id="admin-content">
+        <div id="welcome-admin">
+          <span>Welcome, </span>
+          <span>Rika Furude</span>
+        </div>
+        <div id="admin-content-container">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
